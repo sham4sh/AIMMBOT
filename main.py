22e3fbe2c5e6515b9bc7428686f8ca2c728e9776
bforrest1240 <bforrest1@gmail.com>
2022-11-17 14:56:13 -0500

    Please check to make sure it works

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ class MainWindow(QMainWindow):
         self.setGeometry(100, 100, 1500, 800)
         self.logoText = QLabel("AIMMBOT", parent=self)
         self.logoText.move(10, 30)
-		
+        
         container = QWidget()
         containerLayout = QVBoxLayout()
         container.setLayout(containerLayout)
@@ -65,106 +65,106 @@ class MainWindow(QMainWindow):
 #Window that allows existing users to log in to their account
 class LoginWindow(QWidget):
 
-	def __init__(self):
+    def __init__(self):
 
-		super().__init__()
-		self.setWindowTitle('Login Form')
-		self.resize(500, 120)
+        super().__init__()
+        self.setWindowTitle('Login Form')
+        self.resize(500, 120)
 
-		layout = QGridLayout()
+        layout = QGridLayout()
 
-		label_name = QLabel('<font size="4"> Username </font>')
-		self.lineEdit_username = QLineEdit()
-		self.lineEdit_username.setPlaceholderText('Please enter your username')
-		layout.addWidget(label_name, 0, 0)
-		layout.addWidget(self.lineEdit_username, 0, 1)
+        label_name = QLabel('<font size="4"> Username </font>')
+        self.lineEdit_username = QLineEdit()
+        self.lineEdit_username.setPlaceholderText('Please enter your username')
+        layout.addWidget(label_name, 0, 0)
+        layout.addWidget(self.lineEdit_username, 0, 1)
 
-		label_password = QLabel('<font size="4"> Password </font>')
-		self.lineEdit_password = QLineEdit()
-		self.lineEdit_password.setPlaceholderText('Please enter your password')
-		layout.addWidget(label_password, 1, 0)
-		layout.addWidget(self.lineEdit_password, 1, 1)
+        label_password = QLabel('<font size="4"> Password </font>')
+        self.lineEdit_password = QLineEdit()
+        self.lineEdit_password.setPlaceholderText('Please enter your password')
+        layout.addWidget(label_password, 1, 0)
+        layout.addWidget(self.lineEdit_password, 1, 1)
 
-		button_login = QPushButton('Login')
-		button_login.clicked.connect(self.updateUser)
-		layout.addWidget(button_login, 2, 0, 1, 2)
-		layout.setRowMinimumHeight(2, 75)
+        button_login = QPushButton('Login')
+        button_login.clicked.connect(self.updateUser)
+        layout.addWidget(button_login, 2, 0, 1, 2)
+        layout.setRowMinimumHeight(2, 75)
 
-		button_exit = QPushButton('Return')
-		button_exit.clicked.connect(window.show)
-		button_exit.clicked.connect(self.hide)
-		layout.addWidget(button_exit, 3, 0, 1, 2)
+        button_exit = QPushButton('Return')
+        button_exit.clicked.connect(window.show)
+        button_exit.clicked.connect(self.hide)
+        layout.addWidget(button_exit, 3, 0, 1, 2)
 
-		self.setLayout(layout)
+        self.setLayout(layout)
 
-	def updateUser(self):
+    def updateUser(self):
 
-		currentUser = signin.sign_in_with_email_and_password(email=self.lineEdit_username.text(), password=self.lineEdit_password.text())
-		
-		msg = QMessageBox()
-		try:
-			returnStr = currentUser['error']['message']
-			if(returnStr == 'INVALID_PASSWORD'):
-				returnStr = "Invalid Password"
-			if(returnStr == 'INVALID_EMAIL'):
-				returnStr = "Email Not Recognized"
-			msg.setText(returnStr)
-			msg.exec()
-		except:
-			msg.setText("Logged In Succesfully")
-			msg.exec_()
-			msg.hide()
-			self.hide()
-			window.show()
+        currentUser = signin.sign_in_with_email_and_password(email=self.lineEdit_username.text(), password=self.lineEdit_password.text())
+        
+        msg = QMessageBox()
+        try:
+            returnStr = currentUser['error']['message']
+            if(returnStr == 'INVALID_PASSWORD'):
+                returnStr = "Invalid Password"
+            if(returnStr == 'INVALID_EMAIL'):
+                returnStr = "Email Not Recognized"
+            msg.setText(returnStr)
+            msg.exec()
+        except:
+            msg.setText("Logged In Succesfully")
+            msg.exec_()
+            msg.hide()
+            self.hide()
+            window.show()
 #Window that allows new users to register via Firebase
 class RegisterWindow(QWidget):
 
-	def __init__(self):
+    def __init__(self):
 
-		super().__init__()
-		self.setWindowTitle('Registration Form')
-		self.resize(500, 120)
+        super().__init__()
+        self.setWindowTitle('Registration Form')
+        self.resize(500, 120)
 
-		layout = QGridLayout()
+        layout = QGridLayout()
 
-		label_name = QLabel('<font size="4"> Username </font>')
-		self.lineEdit_username = QLineEdit()
-		self.lineEdit_username.setPlaceholderText('Please enter your username (must be a valid email address)')
-		layout.addWidget(label_name, 0, 0)
-		layout.addWidget(self.lineEdit_username, 0, 1)
+        label_name = QLabel('<font size="4"> Username </font>')
+        self.lineEdit_username = QLineEdit()
+        self.lineEdit_username.setPlaceholderText('Please enter your username (must be a valid email address)')
+        layout.addWidget(label_name, 0, 0)
+        layout.addWidget(self.lineEdit_username, 0, 1)
 
-		label_password = QLabel('<font size="4"> Password </font>')
-		self.lineEdit_password = QLineEdit()
-		self.lineEdit_password.setPlaceholderText('Please enter your password')
-		layout.addWidget(label_password, 1, 0)
-		layout.addWidget(self.lineEdit_password, 1, 1)
+        label_password = QLabel('<font size="4"> Password </font>')
+        self.lineEdit_password = QLineEdit()
+        self.lineEdit_password.setPlaceholderText('Please enter your password')
+        layout.addWidget(label_password, 1, 0)
+        layout.addWidget(self.lineEdit_password, 1, 1)
 
-		button_reg = QPushButton('Register')
-		button_reg.clicked.connect(self.register_user)
-		layout.addWidget(button_reg, 2, 0, 1, 2)
-		layout.setRowMinimumHeight(2, 75)
+        button_reg = QPushButton('Register')
+        button_reg.clicked.connect(self.register_user)
+        layout.addWidget(button_reg, 2, 0, 1, 2)
+        layout.setRowMinimumHeight(2, 75)
 
-		button_exit = QPushButton('Return')
-		button_exit.clicked.connect(window.show)
-		button_exit.clicked.connect(self.hide)
-		layout.addWidget(button_exit, 3, 0, 1, 2)
+        button_exit = QPushButton('Return')
+        button_exit.clicked.connect(window.show)
+        button_exit.clicked.connect(self.hide)
+        layout.addWidget(button_exit, 3, 0, 1, 2)
 
-		self.setLayout(layout)
+        self.setLayout(layout)
 
-	def register_user(self):
+    def register_user(self):
 
-		msg = QMessageBox()
+        msg = QMessageBox()
 
-		try:
-			user = auth.create_user(email=self.lineEdit_username.text(), password=self.lineEdit_password.text())
-			msg.setText("Account Created Succesfully")
-			msg.exec_()
-			msg.hide()
-			self.hide()
-			window.show()
-		except:
-			msg.setText("Username Already Exists")
-			msg.exec()
+        try:
+            user = auth.create_user(email=self.lineEdit_username.text(), password=self.lineEdit_password.text())
+            msg.setText("Account Created Succesfully")
+            msg.exec_()
+            msg.hide()
+            self.hide()
+            window.show()
+        except:
+            msg.setText("Username Already Exists")
+            msg.exec()
 
 #window that allows users to add a favourite movie
 class favoritesWindow(QWidget):
@@ -178,8 +178,8 @@ class favoritesWindow(QWidget):
         button_exit.clicked.connect(window.show)
         button_exit.clicked.connect(self.hide)
         layout.addWidget(button_exit, 0, 0)
-
-        userFavs = FirestoreDataAccess.getFavs("uid") #Need to get the uid
+        
+        userFavs = FirestoreDataAccess.getFavs(FirestoreDataAccess(app=app), "eyJhbGciOiJSUzI1NiIsImtpZCI6IjNmNjcyNDYxOTk4YjJiMzMyYWQ4MTY0ZTFiM2JlN2VkYTY4NDZiMzciLCJ0eXAiOiJKV1QifQ") #Need to get the uid
 
         row = 1
         column = 0
