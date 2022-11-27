6895e57a36f3fddf14e9330a6adcb3cb3e2185dc
kamleenb <kbassi1@pride.hofstra.edu>
2022-11-26 23:05:12 -0500

    turned algorithm.py into a class

--- algorithm.py
+++ algorithm.py
@@ -1,60 +1,79 @@
-from surprise import Prediction, Reader, Dataset,accuracy #,similarities
+from surprise import Reader, Dataset 
 import pandas as pd
 import surprise
-from surprise.model_selection import cross_validate, train_test_split
+#from surprise.model_selection import cross_validate, train_test_split
 from collections import defaultdict
+import numpy as np
 
 
-df = pd.read_csv('data/ratings.csv')
-df.drop('timestamp',axis=1, inplace=True)
-#print(df.head())
+# NOTES:
+    # Add all new users to the filelocation. 
+    # Then instantiate this class or run processData function 
+    # then use get_top_n() to get the predictions 
+class PrimaryAlgorithm:
+    uid = None 
+    data = None
+    filelocation = 'data/ratings.csv'
 
-reader = Reader(rating_scale=(0.5,5.0)) # used to parse file containing ratings - REQUIRED 
+    def __init__(self) -> None:
+        df = self.processData()
 
-data = Dataset.load_from_df(df[['userId','movieId','rating']],reader)
+    def getDataHelper(self): # HELPER FUNCTION 
+        try:
+            df = pd.read_csv(self.filelocation)
+            if np.in1d(np.array(['userId','movieId','rating']), df.columns).all():
+                df = df[['userId','movieId','rating']]
+        except FileNotFoundError as e:
+            print("Prim Algorithm - File Not Found - do not return a widget with user based recommendations.")
+        except Exception as e:
+            print(type(e).__name__, e.args)
+        return df 
 
-def displayResults(algo):
-    for i in algo.keys(): print(i, ":", algo[i])
+    def processData(self):
+        df = self.getDataHelper()
+        reader = Reader(rating_scale=(0.5,5.0)) # used to parse file containing ratings - REQUIRED 
+        data = Dataset.load_from_df(df[['userId','movieId','rating']],reader).build_full_trainset()
+        self.data = data
+        return self.data
+    
+    def getPredictionsHelper(self):
+        algo = surprise.SVD()
+        algo.fit(self.data)
+        userData = self.data.build_testset()
+        algo = surprise.SVD()
+        algo.fit(self.data)
+        predictions = algo.test(userData)
+        return predictions
 
-####algorithms = [surprise.SVD, surprise.SlopeOne, surprise.NMF, surprise.NormalPredictor, surprise.KNNBaseline, surprise.KNNBasic, surprise.KNNWithMeans, surprise.KNNWithZScore, surprise.BaselineOnly, surprise.CoClustering]
-algorithms = [surprise.SVD,surprise.KNNBaseline, surprise.BaselineOnly]
-# SVD 
-#svd = cross_validate(surprise.SVD(),data, measures=['RMSE','MAE'],cv=5,verbose=True)
-#displayResults(svd)
-'''
-for a in algorithms:
-    algo = cross_validate(a(),data, measures=['RMSE','MAE'],cv=3,verbose=True)
-    displayResults(algo)
-'''
-# SVD 
-#svd = cross_validate(.,data, measures=['RMSE','MAE'],cv=5,verbose=True)
-#displayResults("SVD algorithm Error ", svd)
+    def get_top_n(self, uid, n=10):
+        uid = str(uid)
+        predictions = self.getPredictionsHelper()
+        # First map the predictions to each user.
+        top_n = defaultdict(list)
+        for uid, iid, true_r, est, _ in predictions:
+            top_n[uid].append((iid, est))
 
-trainset, testset = train_test_split(data, test_size=0.25)
-algo = surprise.SVD()
-algo.fit(trainset)
-predictions = algo.test(testset)
+        # Then sort the predictions for each user and retrieve the k highest ones.
+        for uid, user_ratings in top_n.items():
+            user_ratings.sort(key=lambda x: x[1], reverse=True)
+            top_n[uid] = user_ratings[:n]
+        
+        if uid in top_n.keys():
+            return([mid for (mid, _) in top_n[uid]])
+        else: return list()
 
-# Then compute RMSE
-#print(accuracy.rmse(predictions))
-accuracy.rmse(predictions)
+    def test(self):
+        prim = PrimaryAlgorithm()
+        df = prim.processData()
+        top_n = prim.get_top_n('1', 10)
+        print(prim.get_top_n('1',15))
+#END CLASS 
 
-def get_top_n(predictions, n=10):
-    #Return the top-N recommendation for each user from a set of predictions.
 
-    # First map the predictions to each user.
-    top_n = defaultdict(list)
-    for uid, iid, true_r, est, _ in predictions:
-        top_n[uid].append((iid, est))
+def getImdbId(mid):
+    mid = int(mid)
+    df = pd.read_csv('data/links.csv')
+    assert 'movieId' in df.columns and 'imdbId' in df.columns
+    return df.loc[mid,'imdbId']
 
-    # Then sort the predictions for each user and retrieve the k highest ones.
-    for uid, user_ratings in top_n.items():
-        user_ratings.sort(key=lambda x: x[1], reverse=True)
-        top_n[uid] = user_ratings[:n]
-    return top_n
-
-top_n = get_top_n(predictions, n=10)
-
-# Print the recommended items for each user
-for uid, user_ratings in top_n.items():
-    print(uid, [mid for (mid, _) in user_ratings]) # mid is movie id 
+print(getImdbId('1'))
